dd5b9e300e4c37c318f9ec4fb96ecdf1885fbe51
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-07-10 13:51:07 +0200

    action: add error contexts

--- src/action.rs
+++ src/action.rs
@@ -1,5 +1,6 @@
 //! Available service actions
 
+use crate::anyhow::Context;
 use crate::{config, github, graph_schema};
 
 use anyhow::Error;
@@ -53,14 +54,17 @@ pub async fn perform_action(
     Credentials::Token(settings.token.clone()),
   )?;
 
-  let tmpdir = tempdir()?;
+  let tmpdir = tempdir().context("Failed to create tempdir")?;
   let path = tmpdir.path().to_path_buf();
 
   debug!(
     "Cloning {}/{} to {}",
     settings.fork_organization.clone(),
     settings.fork_repo.clone(),
-    path.to_str().unwrap().clone(),
+    path
+      .to_str()
+      .context("Failed to convert path to string")?
+      .clone(),
   );
   let repo = github::refresh_forked_repo(
     settings.target_organization.clone(),
@@ -72,13 +76,14 @@ pub async fn perform_action(
 
   debug!("Calculating action");
   match action.r#type {
-    ActionType::Disable => graph_schema::block_edge(&path, action.version.clone())?,
-    ActionType::Enable => graph_schema::unblock_edge(&path, action.version.clone())?,
-  };
+    ActionType::Disable => graph_schema::block_edge(&path, action.version.clone()),
+    ActionType::Enable => graph_schema::unblock_edge(&path, action.version.clone()),
+  }
+  .context("Failed to perform action")?;
 
   let branch = generate_branch_name(action.title.clone());
   debug!("Generated branch {}", branch.clone());
-  github::switch_to(&repo, branch.clone())?;
+  github::switch_to(&repo, branch.clone()).context("Failed to switch to branch")?;
 
   debug!(
     "Pushing to {}/{}",
@@ -86,8 +91,8 @@ pub async fn perform_action(
     settings.fork_repo.clone(),
   );
   let commit_message = format!("{}\n{}", action.title, action.body);
-  github::commit(&repo, branch.clone(), commit_message)?;
-  github::push_to_remote(&repo, branch.clone())?;
+  github::commit(&repo, branch.clone(), commit_message).context("Failed to commit changes")?;
+  github::push_to_remote(&repo, branch.clone()).context("Failed to push to remote")?;
 
   debug!("Preparing pull request");
   let pr_url = github::create_pr(
@@ -99,7 +104,8 @@ pub async fn perform_action(
     action.title,
     action.body,
   )
-  .await?;
+  .await
+  .context("Failed to create PR")?;
   debug!("Created PR {}", pr_url);
 
   Ok(pr_url)
